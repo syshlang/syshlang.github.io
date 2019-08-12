2caa6d100bdf2db7621cd80caebb3620fbd544af
syshlang <sunyslang@gmail.com>
2019-08-12 14:11:51 +0800

    Site updated: 2019-08-12 14:11:39

--- js/schemes/pisces.js
+++ js/schemes/pisces.js
@@ -1,3 +1,3 @@
-// build time:Mon Aug 12 2019 13:54:11 GMT+0800 (GMT+08:00)
+// build time:Mon Aug 12 2019 14:11:35 GMT+0800 (GMT+08:00)
 $(document).ready(function(){var t=$(".sidebar-inner");var i=CONFIG.sidebar.offset||12;function e(){return $(".header-inner").height()+i}function o(){var t=$("#footer");var i=$(".footer-inner");var e=t.outerHeight()-i.outerHeight();var o=t.outerHeight()+e;return o}function a(){var a=e();var f=o();t.affix({offset:{top:a-i,bottom:f}});t.affix("checkPosition");$("#sidebar").css({"margin-top":a,"margin-left":"auto"})}function f(){$(window).off(".affix");t.removeData("bs.affix").removeClass("affix affix-top affix-bottom");a()}function r(){var t=window.matchMedia("(min-width: 992px)");t.addListener(function(t){if(t.matches){f()}})}a();r()});
 //rebuild by neat 
